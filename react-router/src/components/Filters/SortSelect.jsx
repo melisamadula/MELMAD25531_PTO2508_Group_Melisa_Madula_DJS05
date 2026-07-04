d3042f4d17f5d2180732b4384a29db86ff45d3b3
melisamadula <melisamadula.mm@gmail.com>
2026-07-04 23:06:21 +0200

    Fixed/ Updated import paths across files, so code renders app (DJS04 Solution) within new modularization system

--- react-router/src/components/Filters/SortSelect.jsx
+++ react-router/src/components/Filters/SortSelect.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { SORT_OPTIONS, PodcastContext } from "../context/PodcastContext";
+import { SORT_OPTIONS, PodcastContext } from "../../context/PodcastContext";
 import styles from "./SortSelect.module.css";
 
 /**
